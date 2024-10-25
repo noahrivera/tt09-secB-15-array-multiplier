d3f5c21b3c59bebb56174b7d85a9c016f1fe42b4
Noah Rivera <nr2953@ad.nyu.edu>
2024-10-25 13:39:54 -0400

    W

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_secB_15_array_multiplier (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,33 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  
+  wire [3:0] m = ui_in[7:4];
+  wire [3:0] q = ui_in[3:0];
+  wire [7:0] p;
+
+  always @(*) begin
+      p = 0; // Initialize product to 0
+      for (integer i = 0; i < 4; i = i + 1) begin
+          if (q[i]) begin
+              p = p + (m << i); // Shift and add
+          end
+      end
+  end
+
+  assign uo_out = p;
+  
   assign uio_out = 0;
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, uio_in, 1'b0};
 
 endmodule
+
+module array_mult_structural (
+    input [3:0} m,
+    input [3:0} q,
+    output reg [7:0} p
+);
--- test/test.py
+++ test/test.py
@@ -26,15 +26,34 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    dut.ui_in.value = 0b00010001
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    assert dut.uo_out.value == 0b00000001
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+    #T2
+    dut.ui_in.value = 0b00010001
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00000100
+
+    #T3
+    dut.ui_in.value = 0b00011000
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00001000
+
+    #T4
+    dut.ui_in.value = 0b11110001
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b00001111
+
+    #T5
+    dut.ui_in.value = 0b11111111
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 0b11100001
